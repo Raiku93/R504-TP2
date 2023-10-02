7829a75796efa730ef5443161006e0a186810adf
Raiku93 <119341000+Raiku93@users.noreply.github.com>
2023-10-02 12:37:06 +0200

    configuration du fichier de test

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -1,3 +1,9 @@
 import unittest
 from FizzBuzz import *
 
+class TestFizzBuzz(unittest.TestCase):
+    def setUp(self):
+         self.instance=FizzBuzz()
+
+if __name__ == 'main':
+     unittest.main()
