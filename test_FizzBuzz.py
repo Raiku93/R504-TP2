3fd7f5869da060fdfb0f76eb8a13728b7c10fca6
Raiku93 <119341000+Raiku93@users.noreply.github.com>
2023-10-03 17:29:19 +0200

    Test deux param v1 OK

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -5,9 +5,11 @@ class TestFizzBuzz(unittest.TestCase):
     def setUp(self):
          self.instance=FizzBuzz()
 
-    def test_affiche_sans_param(self):
-         nombre = 15
-         self.assertEqual(self.instance.affiche(nombre), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
+    def test_affiche_deux_param(self):
+         nombre1 = 5
+         nombre2 = 10
+         self.assertEqual(self.instance.affiche(nombre1, nombre2), "BuzzFizz78FizzBuzz")
+    
 
 
 if __name__ == '__main__':
