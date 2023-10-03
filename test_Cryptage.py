ab06a31ff47724a9e8d61578f0c74723575f1d66
Raiku93 <119341000+Raiku93@users.noreply.github.com>
2023-10-03 17:43:57 +0200

    Création du teste de l'objet Cryptage

---
+++ test_Cryptage.py
@@ -0,0 +1,12 @@
+import unittest
+from Cryptage import *
+
+class Cryptage_Test(unittest.TestCase):
+    def setUp(self):
+        self.instance=Cryptage()
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
